88ee2fbd6e134e66c48e3b9a8b5386a5723d6734
Wallisson Santos <wallyssonsantos@yahoo.com.br>
2024-12-16 04:59:07 -0300

    criando as classe Atividade e Categoria

---
+++ src/main/java/com/devsuperior/desafioorm/etities/Atividade.java
@@ -0,0 +1,69 @@
+package com.devsuperior.desafioorm.etities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Atividade categoria;
+
+    public Atividade() {
+    }       
+
+    public Atividade(Long id, String nome, String descricao, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
---
+++ src/test/java/com/devsuperior/desafioorm/DesafioormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
